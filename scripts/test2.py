df447a8f9ceba242e1158ae0c1c6acba0a52d6fd
Curtis Maloney <curtis@tinbrain.net>
2015-05-26 09:26:51 +1000

    Gussy up output of test2 script

--- scripts/test2.py
+++ scripts/test2.py
@@ -3,18 +3,23 @@ from client import Keyser
 
 k = Keyser()
 
+print "SET values"
 for n in xrange(100000):
     key = '%08d' % n
     val = '%20d' % n
 
-    print 'Set: %r == %r' % (key, val)
+    print '%r == %r\r' % (key, val),
     k[key] = val
 
+print "\nGET values"
 for n in xrange(100000):
     key = '%08d' % n
 
-    print '%s == %s' % (key, k[key])
+    val = k[key]
+    assert val == '%20d' % n
+    print '%r == %r\r' % (key, val),
 
+print "\nDEL values"
 for n in xrange(100000):
     key = '%08d' % n
 
